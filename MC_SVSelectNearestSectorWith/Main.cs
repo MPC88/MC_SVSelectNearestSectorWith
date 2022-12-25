16a3d0561d45ecec20e52ee2b2b9f017c9b83d11
MPC88 <myemail@gmail.com>
2022-12-25 12:08:25 +0000

    Add project files.

---
+++ MC_SVSelectNearestSectorWith/Main.cs
@@ -0,0 +1,160 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace MC_SVSelectClosestSectorWith
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        public const string pluginGuid = "mc.starvalor.selectclosestsectorwithquest";
+        public const string pluginName = "SV Select Closest Sector with...";
+        public const string pluginVersion = "1.0.0";
+
+        private static GameObject questButton;
+        private static GameObject ravagerButton;
+
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(Main));
+        }
+
+        [HarmonyPatch(typeof(GalaxyMap), nameof(GalaxyMap.ShowHideGalaxyMap))]
+        [HarmonyPostfix]
+        private static void GMShowHide_Pre(GalaxyMap __instance, GameObject ___BtnWarp, Slider ___sliderFactionInfluence)
+        {
+            if (questButton == null)
+            {
+                questButton = Instantiate(___BtnWarp);
+                questButton.name = "BtnFindNearestQuest";
+                Destroy(questButton.transform.Find("Cost").gameObject);
+                questButton.SetActive(true);
+                questButton.GetComponentInChildren<Text>().text = "Nearest Quest";
+                questButton.SetActive(false);
+                Button.ButtonClickedEvent btnClickEvent = new Button.ButtonClickedEvent();
+                btnClickEvent.AddListener(new UnityAction(QuestBtnClick));
+                questButton.GetComponentInChildren<Button>().onClick = btnClickEvent;
+                questButton.transform.SetParent(___sliderFactionInfluence.transform.parent);
+                questButton.layer = ___sliderFactionInfluence.gameObject.layer;
+                questButton.transform.localPosition = new Vector3(
+                    ___sliderFactionInfluence.transform.localPosition.x - (___BtnWarp.GetComponentInChildren<RectTransform>().rect.x),
+                    ___sliderFactionInfluence.transform.localPosition.y + (___BtnWarp.GetComponentInChildren<RectTransform>().rect.y * 2),
+                    ___sliderFactionInfluence.transform.localPosition.z
+                    );
+                questButton.transform.localScale = ___sliderFactionInfluence.transform.localScale;
+            }
+
+            if (ravagerButton == null)
+            {
+                ravagerButton = Instantiate(___BtnWarp);
+                ravagerButton.name = "BtnFindNearestRavager";
+                Destroy(ravagerButton.transform.Find("Cost").gameObject);
+                ravagerButton.SetActive(true);
+                ravagerButton.GetComponentInChildren<Text>().text = "Nearest Ravager";
+                ravagerButton.SetActive(false);
+                Button.ButtonClickedEvent btnClickEvent = new Button.ButtonClickedEvent();
+                btnClickEvent.AddListener(new UnityAction(RavagerBtnClick));
+                ravagerButton.GetComponentInChildren<Button>().onClick = btnClickEvent;
+                ravagerButton.transform.SetParent(questButton.transform.parent);
+                ravagerButton.layer = questButton.gameObject.layer;
+                ravagerButton.transform.localPosition = new Vector3(
+                    ___sliderFactionInfluence.transform.localPosition.x - (___BtnWarp.GetComponentInChildren<RectTransform>().rect.x),
+                    questButton.transform.localPosition.y + (___BtnWarp.GetComponentInChildren<RectTransform>().rect.y * 1.1f),
+                    questButton.transform.localPosition.z
+                    );
+                ravagerButton.transform.localScale = questButton.transform.localScale;
+            }
+
+            questButton.SetActive(__instance.gameObject.activeSelf);
+            ravagerButton.SetActive(__instance.gameObject.activeSelf);
+        }
+
+        private static void RavagerBtnClick()
+        {
+            if (GalaxyMap.instance == null)
+                return;
+
+            TSector curSector = GameData.data.GetCurrentSector();
+            TSector closestSector = null;
+            float distance = 0;
+
+            for (int i = 0; i < GameData.data.sectors.Count; i++)
+            {
+                if(GameData.data.sectors[i].discovered && 
+                    GameData.data.sectors[i].boss != null && 
+                    GameData.data.sectors[i].boss.alive &&
+                    GameData.data.sectors[i] != curSector)
+                {
+                    float tempDist = Mathf.Abs(Vector2.Distance(curSector.realPosV2, GameData.data.sectors[i].realPosV2));
+                    if(distance == 0 || tempDist < distance)
+                    {
+                        closestSector = GameData.data.sectors[i];
+                        distance = tempDist;
+                    }
+                }
+            }
+
+            if (closestSector == null)
+                InfoPanelControl.inst.ShowWarning("No ravager found.", 1, false);
+            else
+                GalaxyMap.instance.MoveCameraTo(closestSector, true);
+        }
+
+        private static void QuestBtnClick()
+        {
+            if (GalaxyMap.instance == null)
+                return;
+
+            TSector curSector = GameData.data.GetCurrentSector();
+            TSector closestQuestSector = null;
+            float distance = 0;
+            for (int i = 0; i < PChar.Char.activeQuests.Count; i++)
+            {
+                Quest q = PChar.Char.activeQuests[i];
+                if (q.objectives.Length > 0)
+                {
+                    TSector questObjSector = GetQuestSector(q.objectives[0]);
+                    if (questObjSector != null && questObjSector != curSector)
+                    {
+                        if (closestQuestSector == null)
+                        {
+                            closestQuestSector = questObjSector;
+                            distance = Mathf.Abs(Vector2.Distance(curSector.realPosV2, questObjSector.realPosV2));
+                        }
+                        else
+                        {
+                            float tempDist = Mathf.Abs(Vector2.Distance(curSector.realPosV2, questObjSector.realPosV2));
+                            if (tempDist < distance)
+                            {
+                                distance = tempDist;
+                                closestQuestSector = questObjSector;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (closestQuestSector == null)
+                InfoPanelControl.inst.ShowWarning("No quest sector found.", 1, false);
+            else
+                GalaxyMap.instance.MoveCameraTo(closestQuestSector, true);
+        }
+
+        private static TSector GetQuestSector(QuestObjective questObjective)
+        {
+            if (questObjective.type == QuestObjectiveType.GoToCoordenates)
+                return GameData.data.sectors[questObjective.par1];
+            else if (questObjective.type == QuestObjectiveType.GoToSector)
+                return GameData.data.GetExistingSector(questObjective.par1, questObjective.par2);
+            else if (questObjective.type == QuestObjectiveType.GoToStation)
+                return GameData.data.sectors[StationSystem.GetStation(questObjective.par1, true).sectorIndex];
+            else if (questObjective.type == QuestObjectiveType.Clear_Eliminate)
+                return GameData.data.sectors[questObjective.par1];
+
+            return null;
+        }
+    }
+}
---
+++ MC_SVSelectNearestSectorWith/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MC_SVSelectNearestSectorWith")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MC_SVSelectNearestSectorWith")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9a9e9a5e-c717-4f7f-8a87-51ff0a6d6d5c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
